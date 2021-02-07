8d96d37a915d1bfc1c918453177fddcaad203296
JoelSaleem <joeljsaleem@gmail.com>
2021-02-07 16:31:14 +0000

    :loud_sound: add rich for logging

--- search.py
+++ search.py
@@ -1,9 +1,20 @@
 import json
+from rich.console import Console
+from rich.table import Table
+from rich.progress import track
 from results import Results
 from util import (
     calculate_score, parse_args,
     parse_str_to_tokens, get_target_str
 )
+
+# TODO: refactor out
+console = Console()
+table = Table(show_header=True, header_style="bold magenta")
+table.add_column("Score", width=12)
+table.add_column("id", style="dim")
+table.add_column("name", style="dim")
+table.add_column("brand", style="dim")
 
 if __name__ == "__main__":
     query, dataset = parse_args()
@@ -14,11 +25,21 @@ if __name__ == "__main__":
 
     results = Results(max_size=10)
     query_tokens = parse_str_to_tokens(query)
-    for item in data:
+    for i in track(range(len(data))):
+        item = data[i]
         target = get_target_str(item)
         target_tokens = parse_str_to_tokens(target)
 
         score = calculate_score(target_tokens, query_tokens)
         results.add_item({'score': score, 'item': item})
 
-    print(results)
+    results.sort(key=lambda r: r['score'], reverse=True)
+    for r in results:
+        table.add_row(
+            str(r['score']),
+            str(r['item']['id']),
+            r['item']['name'],
+            r['item']['brand']
+
+        )
+    console.log(table)
